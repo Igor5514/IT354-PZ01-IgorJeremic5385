d1752abcbdb487377a5d6feb9cdf1c24c5525677
BuildTools <igorjeremic128@gmail.com>
2025-03-02 18:41:41 +0100

    shop update

--- IT354-PZ/src/pages/Shop.jsx
+++ IT354-PZ/src/pages/Shop.jsx
@@ -2,13 +2,21 @@ import FirstCard from "../shop_components/FirstCard";
 import image from "../images/slika.jpg";
 import "./pages.css"
 import CarSelectionMenu from "../shop_components/CarSelectionMenu.jsx";
+import image2 from "../images/service.jpg";
 
 export const Shop = () => {
     let list = ["asdasdasd", "bobasdasd", "asdasdasd", "asdsasdasd"];
     return (
         <>
-            <div>
-                <CarSelectionMenu />
+            <div className="main-shop-container">
+                <div className="main-menu-container">
+                    <CarSelectionMenu />
+                    <div className="image-container">
+                        <img src={image2} />
+                        <p className="image-title">Auto Parts & Accessories</p>
+                    </div>
+                </div>
+                <h1 className="shop-title">Order best quality parts online</h1>
                 <div className="part-carts-grid-container">
                     <FirstCard title={"title"} paragraphs={list} img={image}/>
                 </div>
